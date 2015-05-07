80c111092348ea920ab0b4c571bce624d0125f32
knutk <knut@kolvig.no>
2015-05-07 21:25:29 +0200

    Updates legend in plot 3

--- plot3.R
+++ plot3.R
@@ -14,6 +14,6 @@ with(data, plot(datetime, Sub_metering_1, type="l", xlab="", ylab = "Energy sub 
 with(data, lines(datetime, Sub_metering_2, col="red"))
 with(data, lines(datetime, Sub_metering_3, col="blue"))
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       col=c("black", "red", "blue"), lty=1)
+       col=c("black", "red", "blue"), lty=1, lwd=2)
 
 dev.off()
